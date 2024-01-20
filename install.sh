ff4b543e0ff375f4f76e9fc7ef533fafdb995791
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-01-20 17:22:46 -0500

    useradd

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/env sh
 
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
-sudo ln -s "$dir/bot-girl-technology.service" /etc/systemd/system/bot-girl-technology.service
+sudo ln -s "$dir/bot-github_webhook_watcher.service" /etc/systemd/system/bot-github_webhook_watcher.service
+sudo useradd -m -U bot-github_webhook_watcher -G ubuntu
