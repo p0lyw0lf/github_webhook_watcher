f49ccaa9f1d9ffc6f71452cdb741b03100bd1f11
p0lyw0lf <p0lyw0lf@protonmail.com>
2024-01-20 07:48:02 -0500

    validate webhook

--- config.py
+++ config.py
@@ -1,13 +1,34 @@
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Optional
 
 
-# The (relative) path of the repository you want to have updated
-REPOSITORY_LOCAL = Path(".")
+@dataclass
+class Repository:
+    """
+    The (relative) path of the repository you want to have updated
+    """
+    local: Path
+    """
+    The branch name you want to track
+    """
+    branch: str
+    """
+    The script to run after an update occurs
+    Set to `None` to not run anything
+    """
+    after_update: Optional[Path]
 
-# The URL of the github repository you want
-# TODO: what's the format for the `repository` in the webhook? to be seen.
-REPOSITORY_REMOTE = "https://github.com/p0lyw0lf/github_webhook_watcher.git"
 
-# The script to run after an update occurs
-# Set to `None` to not run anything
-AFTER_UPDATE = None
+"""
+Maps repository remotes to configurations
+TODO: what's the format for the `repository` in the webhook? to be seen.
+"""
+REPOSITORIES: dict[str, Repository] = {}
+"""
+The secret value you share with GitHub to authenticate webhook requests
+See
+https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
+for more information
+"""
+SECRET: str = b"secret"
--- server.py
+++ server.py
@@ -2,24 +2,39 @@ from sanic import Sanic
 from sanic.request import Request
 from sanic.response import text, HTTPResponse
 from sanic.log import logger
+import hashlib
+import hmac
 
 app = Sanic("github_webhook_watcher")
 app.update_config("./config.py")
 
 
+def verify_signature(body: bytes, signature: str) -> bool:
+    if not signature:
+        return False
+    hash_object = hmac.new(app.config.SECRET,
+                           msg=body,
+                           digestmod=hashlib.sha256)
+    expected_signature = f"sha256={hash_object.hexdigest()}"
+
+    return hmac.compare_digest(expected_signature, signature)
+
+
 @app.route("/")
 async def index(request):
-    logger.info("index")
     return text("Hello, world.")
 
 
 @app.route("/webhook", methods=["POST"])
 async def webhook(request: Request) -> HTTPResponse:
-    github_event = request.headers["x-github-event"]
+    signature = request.headers["x-hub-signature-256"]
+    if not verify_signature(request.body, signature):
+        return text("Invalid Signature", 403)
 
+    github_event = request.headers["x-github-event"]
     if github_event == "push":
         payload = request.json
-        commit = payload.after
-        logger.info(f"Newest commit: {commit}")
+        repository = payload.get("repository", None)
+        logger.info(f"{repository=}")
 
     return text("Accepted", 202)
